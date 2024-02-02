f17b44b208b78ab23d412ea4ccbec6e9994e04a6
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2024-02-03 05:15:54 +0800

    Create page1.java

---
+++ Interactive Story Book/page1.java
@@ -0,0 +1,37 @@
+package com.example.interactivestoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+
+public class page1 extends AppCompatActivity {
+    EditText et_MainCharacter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et_MainCharacter = findViewById(R.id.MainCharacter);
+    }
+
+    public void proceed(View view) {
+        String mainCharacter = et_MainCharacter.getText().toString().trim();
+
+        if (mainCharacter.isEmpty()) {
+            // If main character is empty, show toast and return
+            Toast.makeText(this, "Please Enter your Main Character", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Toast.makeText(this, "Main Character set as: " + mainCharacter , Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, page1.class);
+        i.putExtra("pangalan", mainCharacter);
+        startActivity(i);
+    }
+
+}
